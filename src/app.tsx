beb93f2c2fae3a85c602e277d2202a7ce6e74bcf
Barnabás BARTHA <barnabas@barthaweb.com>
2022-10-09 11:06:14 +0200

    Add tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
 import React from 'react';
 
-export const App = () => <>TODO</>;
+export const App = () =>
+    <div className="flex h-screen bg-[#EEEEEE] overflow-x-hidden">
+        <div className="w-full" />
+        <div className="w-[900px] h-full bg-white shrink-0">
+            RIGHT
+        </div>
+    </div>;
